3a4bac6b341f2510a37ace132d1b3b4ce3713280
Kyle Robinson Young <kyle@dontkry.com>
2014-03-22 10:18:46 -0700

    Stub hasMany and build belongsTo

--- index.js
+++ index.js
@@ -52,6 +52,44 @@ LevelREST.prototype.get = function(api, params) {
     out.end()
   })
 
+  // Attach hasMany method
+  out.hasMany = function(model) {
+    // TODO: Build this
+    //self.db.sublevel(model)
+    console.log(model)
+    return this
+  }
+
+  // Attach belongsTo method
+  out.belongsTo = function(model) {
+    var singular = inflect.singularize(model)
+    var key = singular + '_id'
+    var db = self.db.sublevel(model)
+    return out.pipe(map(function(data, cb) {
+      if (api.id) {
+        // belongsTo with one
+        data[singular] = {}
+        if (!data[api.singular][key]) return cb(null, data)
+        db.get(data[api.singular][key], function(err, d) {
+          if (err) return cb(null, data)
+          data[singular] = d
+          cb(null, data)
+        })
+      } else {
+        // belongsTo with many
+        var len = data[api.api].length
+        function done() { len--; if (len < 1) cb(null, data) }
+        data[api.api].forEach(function(item, i) {
+          var foreignKey = data[api.api][i][key]
+          db.get(foreignKey, function(err, d) {
+            data[api.api][i][singular] = d || {}
+            done()
+          })
+        })
+      }
+    }))
+  }
+
   return out
 }
 
